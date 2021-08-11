4ca995ae9682d09488c2a225dfb212847ae01bfb
Lucas Pedroso <lucasmendoncawf@gmail.com>
2021-08-11 00:08:46 -0300

    [MCNT-3219] correção de testes

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -9,9 +9,10 @@ export class AboutComponent implements OnInit {
 
   members = [
     { name: 'Lucas Mendonça', photo: 'pessoax.jpg', position: 'CEO, Diretor e Roteirista'},
-    { name: 'Guilherme Dunguel', photo: 'pessoax.jpg', position: 'Designer e Analista de marketing'},
+    { name: 'Guilherme Dunguel', photo: 'pessoax.jpg', position: 'Designer e Analista de Marketing'},
     { name: 'Caio Balbino', photo: 'pessoax.jpg', position: 'Desenhista'},
     { name: 'Lucas Freire', photo: 'pessoax.jpg', position: 'Designer e Documentador'},
+    { name: 'Daniel Galuppo', photo: 'pessoax.jpg', position: 'Editor de Vídeo e Motion Designer'},
   ];
   constructor() { }
 
